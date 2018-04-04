0080a2bc63b3364ec56e5ddbf1b9dfc704e42b6f
Owner <owner@MacBook-Pro.local>
2018-04-04 14:24:27 -0400

    will expand on Node

--- react/App.jsx
+++ react/App.jsx
@@ -4,11 +4,13 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 import Landing from "./Landing";
+import Done from "./Done";
 
 const App = () => (
   <Provider store={store}>
     <div className="app">
       <Route exact path="/" component={props => <Landing />} />
+      <Route exact path="/done" component={props => <Done />} />
     </div>
   </Provider>
 );
--- react/Validation.jsx
+++ react/Validation.jsx
@@ -1,14 +1,85 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Validation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phoneNumber: ""
+    };
+
+    this._handlePhoneNumber = this._handlePhoneNumber.bind(this);
+    this._handleFirstName = this._handleFirstName.bind(this);
+    this._handleLastName = this._handleLastName.bind(this);
+    this._handleEmail = this._handleEmail.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+
+  _checkTypeAndSetState(value, state, type) {
+    if (typeof value === type) {
+      this.setState({
+        [state]: value
+      });
+    }
+  }
+
+  _handlePhoneNumber(event) {
+    this._checkTypeAndSetState(event.target.value, "phoneNumber", "string");
+  }
+
+  _handleFirstName(event) {
+    this._checkTypeAndSetState(event.target.value, "firstName", "string");
+  }
+
+  _handleLastName(event) {
+    this._checkTypeAndSetState(event.target.value, "lastName", "string");
+  }
+
+  _handleEmail(event) {
+    this._checkTypeAndSetState(event.target.value, "email", "string");
+  }
+
+  _handleSubmit(event) {
+    event.preventDefault();
+
+    console.log("submitting");
+  }
+
   render() {
+    const { firstName, lastName, email, phoneNumber } = this.state;
+    const {
+      _handleEmail,
+      _handleFirstName,
+      _handleLastName,
+      _handlePhoneNumber,
+      _handleSubmit
+    } = this;
+
+    console.log("this.", this.state);
     return (
-      <div className="validation-grid">
-        <input placeholder="First Name" />
-        <input placeholder="Last Name" />
-        <input placeholder="Email" />
-        <input placeholder="Phone Number" />
-        <button>Submit</button>
+      <div>
+        <form className="validation-grid" onSubmit={_handleSubmit}>
+          <div>
+            <h3>First Name </h3>
+            <input value={firstName} onChange={_handleFirstName} />
+          </div>
+          <div>
+            <h3>Last Name </h3>
+            <input value={lastName} onChange={_handleLastName} />
+          </div>
+          <div>
+            <h3>Email </h3>
+            <input value={email} onChange={_handleEmail} />
+          </div>
+          <div>
+            <h3>Phone Number </h3>
+            <input value={phoneNumber} onChange={_handlePhoneNumber} />
+          </div>
+          <button><Link to="/done">Submit</Link></button>
+        </form>
       </div>
     );
   }
